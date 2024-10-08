427bd7fb797e6cc6d0ecaeb6c7ce74a26c9a5734
Spncr-Space <spencerbformacion@gmail.com>
2024-10-09 07:54:24 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  string word;
+
+
+  cout << "Type a word: "; // Type a number and press enter
+  cin >> word; // Get user input from the keyboard
+  cout << "Your word is: " << word;
+  return 0;
+  }
